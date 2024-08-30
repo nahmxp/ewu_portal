b09d63bddba40b1a3e204a47495379ce714d57cf
nahmed117 <nahmed117m@gmail.com>
2024-08-30 20:57:40 +0600

    app generated in build,app,output,real folder

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@
 // ignore_for_file: public_member_api_docs
 
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
@@ -22,94 +21,7 @@ import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 void main() => runApp(const MaterialApp(debugShowCheckedModeBanner: false,home: WebViewExample()));
 
 
-const String kNavigationExamplePage = '''
-<!DOCTYPE html><html>
-<head><title>Navigation Delegate Example</title></head>
-<body>
-<p>
-The navigation delegate is set to block navigation to the youtube website.
-</p>
-<ul>
-<ul><a href="https://www.youtube.com/">https://www.youtube.com/</a></ul>
-<ul><a href="https://www.google.com/">https://www.google.com/</a></ul>
-</ul>
-</body>
-</html>
-''';
 
-const String kLocalExamplePage = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Load file or HTML string example</title>
-</head>
-<body>
-
-<h1>Local demo page</h1>
-<p>
-  This is an example page used to demonstrate how to load a local file or HTML
-  string using the <a href="https://pub.dev/packages/webview_flutter">Flutter
-  webview</a> plugin.
-</p>
-
-</body>
-</html>
-''';
-
-const String kTransparentBackgroundPage = '''
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Transparent background test</title>
-  </head>
-  <style type="text/css">
-    body { background: transparent; margin: 0; padding: 0; }
-    #container { position: relative; margin: 0; padding: 0; width: 100vw; height: 100vh; }
-    #shape { background: red; width: 200px; height: 200px; margin: 0; padding: 0; position: absolute; top: calc(50% - 100px); left: calc(50% - 100px); }
-    p { text-align: center; }
-  </style>
-  <body>
-    <div id="container">
-      <p>Transparent background test</p>
-      <div id="shape"></div>
-    </div>
-  </body>
-  </html>
-''';
-
-const String kLogExamplePage = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Load file or HTML string example</title>
-</head>
-<body onload="console.log('Logging that the page is loading.')">
-
-<h1>Local demo page</h1>
-<p>
-  This page is used to test the forwarding of console logs to Dart.
-</p>
-
-<style>
-    .btn-group button {
-      padding: 24px; 24px;
-      display: block;
-      width: 25%;
-      margin: 5px 0px 0px 0px;
-    }
-</style>
-
-<div class="btn-group">
-    <button onclick="console.error('This is an error message.')">Error</button>
-    <button onclick="console.warn('This is a warning message.')">Warning</button>
-    <button onclick="console.info('This is a info message.')">Info</button>
-    <button onclick="console.debug('This is a debug message.')">Debug</button>
-    <button onclick="console.log('This is a log message.')">Log</button>
-</div>
-
-</body>
-</html>
-''';
 
 class WebViewExample extends StatefulWidget {
   const WebViewExample({super.key});
@@ -193,7 +105,7 @@ Page resource error:
 
     // setBackgroundColor is not currently supported on macOS.
     if (kIsWeb || !Platform.isMacOS) {
-      controller.setBackgroundColor(const Color(0x80000000));
+      controller.setBackgroundColor(const Color.fromARGB(255, 255, 255, 255));
     }
 
     // #docregion platform_features
@@ -210,7 +122,7 @@ Page resource error:
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
         title: const Text('EWU Portal'),
         // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
@@ -220,21 +132,6 @@ Page resource error:
         ],
       ),
       body: WebViewWidget(controller: _controller),
-      floatingActionButton: favoriteButton(),
-    );
-  }
-
-  Widget favoriteButton() {
-    return FloatingActionButton(
-      onPressed: () async {
-        final String? url = await _controller.currentUrl();
-        if (mounted) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Shear $url')),//-----add shear option here;
-          );
-        }
-      },
-      child: const Icon(Icons.share_rounded),
     );
   }
 
